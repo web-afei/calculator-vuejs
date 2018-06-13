beb905c97e5403f903b54bfd2af4bac1404b7e5e
AK47-afei <772093633@qq.com>
2018-06-13 16:59:08 +0800

    提交项目

---
+++ README.md
@@ -0,0 +1,30 @@
+# vue-webpack-calculator
+
+> 用vue.js实现简易计算器<br>
+> 基于 vue2.0 + vue-cli +vue-loader.
+<br>
+
+# 演示demo
+<img src="./calculator_vuejs.gif" alt="calculator.vuejs-demo" width="360px" height="auto">
+<br>
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+关于vue-loader更多详细的说明请关注[docs for vue-loader](http://vuejs.github.io/vue-loader).
+<br>
+
+## todo item
+- add keycode eventListener
+<br>
+
+ヾ(o◕∀◕)ﾉヾ 如果有那么一丁点儿喜欢 请随手🌟一下(≧▽≦)/~啦啦啦 <br>
---
+++ src/Calculator.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="calculator">
+    <header>
+      <h1>vue2.0制作计算器</h1>
+      <span class="author">BY <a href="https://web-afei.github.io" target="_blank">web-afei</a></span>
+    </header>
+    <section class="calculator" id="calculator">
+      <calculator-content></calculator-content>
+    </section>
+  </div>
+</template>
+
+<script>
+import CalculatorContent from 'components/CalculatorContent.vue'
+export default {
+  name: 'calculator',
+  components: {
+    CalculatorContent
+  }
+ }
+
+</script>
+
+<style>
+
+html{
+   font-size:62.5%;  /* =10px */
+}
+
+body{
+  background-color:#f4f1f1; /* HEX */
+}
+
+
+header{
+  font-size: 1.8rem;
+  font-family: '微软雅黑';
+}
+
+header .author{
+  font-size: 1.5rem;
+}
+
+header .author a{
+  text-decoration: none;
+}
+
+header , .calculator {
+  margin: 2.5rem auto;
+  text-align: center;
+}
+
+.calculator{
+  border: 0.8rem solid #f67373;
+  width: 34rem;
+  height: 40rem;
+  background-color: #f28080;
+  font-size: 1.6rem;
+  -webkit-border-radius: 1rem;
+  -moz-border-radius: 1rem;
+  -ms-border-radius: 1rem;
+  -o-border-radius: 1rem;
+  border-radius: 1rem;
+  -webkit-box-shadow: 1rem 1rem 0.5rem #ccc;
+  box-shadow: 1rem 1rem 0.5rem #ccc;
+  
+}
+
+
+</style>
